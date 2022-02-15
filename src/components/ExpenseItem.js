6de4c933d6f199cedea810aa931aabff143f5781
zerinri <zerinri.maestralsolutions.com>
2022-02-15 23:43:00 +0100

    Dan 1

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date: 15/02/2022</div>
+      <div>
+        <h2>Title: Test</h2>
+        <div>Amount: 20$</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
