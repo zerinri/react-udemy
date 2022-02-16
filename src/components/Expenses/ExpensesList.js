9fe1053823067d69c0a6696329a4ad0b72484d9f
zerinri <zerinri.maestralsolutions.com>
2022-02-16 15:48:47 +0100

    Day 2

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem"
+
+function ExpensesList(props) {
+  return (
+    <ul className="expenses-list">
+      {props.items.length === 0 ? (
+        <h2 className="expenses-list__fallback">No expenses found.</h2>
+      ) : (
+        props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )}
+    </ul>
+  );
+}
+
+export default ExpensesList;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-function Card(props) {
+import "./Card.css";
+
+const Card = (props) => {
   const classes = "card " + props.className;
+
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
